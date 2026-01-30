6f911d766c2f2f321479b8469af03c4e52faa513
Andrew Levy <alevy@mw-sf.com>
2026-01-30 15:18:27 -0600

    db test

---
+++ src/routes/abc/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { getAbcCodes } from './abc.remote';
+</script>
+
+<ul>
+  {#each await getAbcCodes() as c}
+    <li>
+      <div>
+        <h4>{c.id} - {c.name}</h4>
+        <h4>{c.productLine}</h4>
+      </div>
+    </li>
+  {/each}
+</ul>
---
+++ src/routes/abc/abc.remote.ts
@@ -0,0 +1,18 @@
+import { query } from '$app/server';
+import { env } from '$env/dynamic/private';
+import * as odbc from 'odbc';
+import * as v from 'valibot';
+
+const abcCodeSchema = v.array(v.object({
+  id: v.pipe(v.string(), v.trimEnd()),
+  name: v.pipe(v.string(), v.trimEnd()),
+  productLine: v.pipe(v.string(), v.trimEnd())
+}))
+
+export const getAbcCodes = query(async () => {
+  const connection = await odbc.connect(env.DB_CONNECTION);
+
+  const results = await connection.query('SELECT Id AS id, Name AS name, ProductLineId AS productLine FROM Groups');
+
+  return v.parse(abcCodeSchema, results);
+});
